dbb841c8b0f2fc2e94eb53a1d20f2ef2f6f7dc7a
p0ppy-fl0w3r <kunai_madara@protonmail.com>
2023-01-04 18:59:06 +0545

    Add theme and fonts.
    Added colors, fonts and default theme.

--- app/src/main/java/com/fl0w3r/hydro/MainActivity.kt
+++ app/src/main/java/com/fl0w3r/hydro/MainActivity.kt
@@ -10,17 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.fl0w3r.hydro.ui.theme.HydroBroTheme
+import com.fl0w3r.hydro.ui.theme.HydroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HydroBroTheme {
+            HydroTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
                 }
@@ -40,7 +39,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    HydroBroTheme {
+    HydroTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/fl0w3r/hydro/ui/theme/Color.kt
+++ app/src/main/java/com/fl0w3r/hydro/ui/theme/Color.kt
@@ -1,11 +1,17 @@
 package com.fl0w3r.hydro.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.material.darkColors
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue500 = Color(0xFF1E80B9)
+val DarkBlue900 = Color(0xFF26282F)
+
+// The app is always in dark mode regardless of the user's system theme.
+val ColorPalette = darkColors(
+    primary = Blue500,
+    surface = DarkBlue900,
+    onSurface = Color.White,
+    background = DarkBlue900,
+    onBackground = Color.White
+)
